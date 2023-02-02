3748457fcd1df8f728183590c2b86d352e03750c
ianstrom <iankstrom@gmail.com>
2023-02-02 01:02:20 -0500

    trying to solve stream error

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+    fetch('http://localhost:3000/users', configurationObject)
+        // .then(data => {
+        //     return data.json();
+        // })
+}
